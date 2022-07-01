c2cef8bea03b05df50194b70859bebf754a3e563
Vinicius Aquino <viniciussvl@hotmail.com>
2022-07-01 08:54:03 -0300

    Continuando a estrutura do projeto criando modulos produtos e categorias

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { ToastComponent } from './components/toast/toast.component';
+import { AlertComponent } from './components/alert/alert.component';
 
 @NgModule({
   imports: [
@@ -13,6 +15,8 @@ import { RouterModule } from '@angular/router';
     RouterModule
   ],
   declarations: [
+    ToastComponent,
+    AlertComponent
   ],
   exports: [
     FormsModule,
